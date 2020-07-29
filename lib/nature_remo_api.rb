ed218380089040c1ead7d06d0f98ae34c3aea709
keita <keitahino0415@gmail.com>
2020-07-29 23:41:13 +0900

    first commit

---
+++ lib/nature_remo_api.rb
@@ -0,0 +1,6 @@
+require "nature_remo_api/version"
+
+module NatureRemoApi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/nature_remo_api/version.rb
@@ -0,0 +1,3 @@
+module NatureRemoApi
+  VERSION = "0.1.0"
+end
