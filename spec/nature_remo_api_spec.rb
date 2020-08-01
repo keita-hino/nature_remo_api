ac7f9f601fd8a41330dcfaa04653ac8d39d4da67
keita <keitahino0415@gmail.com>
2020-08-01 21:46:26 +0900

    add TODO comment

--- spec/nature_remo_api_spec.rb
+++ spec/nature_remo_api_spec.rb
@@ -3,7 +3,5 @@ RSpec.describe NatureRemoApi do
     expect(NatureRemoApi::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+  # TODO: Write various API tests here
 end
