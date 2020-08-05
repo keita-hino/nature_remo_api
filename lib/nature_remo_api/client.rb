6abe5f50164a81f79dba5b9710cf5476ba6166cc
keita <keitahino0415@gmail.com>
2020-08-05 22:46:14 +0900

    add API to send signal

--- lib/nature_remo_api/client.rb
+++ lib/nature_remo_api/client.rb
@@ -40,6 +40,14 @@ module NatureRemoApi
       get("/1/appliances/#{appliance_id}/signals")
     end
 
+    def send_signal(signal_id:)
+      params = {
+        signal: signal_id
+      }
+
+      post("/1/signals/#{signal_id}/send", params)
+    end
+
     # TODO: BaseClientを作ってそちらに定義
     Faraday::Connection::METHODS.each do |method|
       define_method(method) do |url, args = {}, &block|
