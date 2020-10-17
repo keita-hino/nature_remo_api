ea537c529cffb0ef1233df4bb4c1b269e628f4e3
keita <keitahino0415@gmail.com>
2020-10-17 10:44:07 +0900

    methodify the client

--- lib/nature_remo_api/client.rb
+++ lib/nature_remo_api/client.rb
@@ -8,26 +8,29 @@ module NatureRemoApi
     include Endpoints
 
     # TODO: NatureRemoApi::Config::ATTRIBUTESを下記に設定できるようにする
-    attr_accessor :endpoint, :access_token, :default_max_retries, :client
+    attr_accessor :endpoint, :access_token, :default_max_retries
 
     def initialize(options = {})
       NatureRemoApi::Config::ATTRIBUTES.each do |key|
         send("#{key}=", options.fetch(key, NatureRemoApi.config.send(key)))
       end
       @access_token ||= NatureRemoApi.config.access_token
-      @client = Faraday.new(url: endpoint) do |conn|
-        conn.request :json
-        conn.response :mashify
-        conn.response :json, :content_type => /\bjson$/
-        conn.adapter Faraday.default_adapter
-      end
-      @client.headers['Authorization'] = "Bearer #{access_token}"
+      client.headers['Authorization'] = "Bearer #{access_token}"
     end
 
     # TODO: BaseClientを作ってそちらに定義
     Faraday::Connection::METHODS.each do |method|
       define_method(method) do |url, args = {}, &block|
         response = client.__send__(method, url, args.reject {|_k, v| v.nil? }).body
+      end
+    end
+
+    def client
+      @client ||= Faraday.new(url: endpoint) do |conn|
+        conn.request :json
+        conn.response :mashify
+        conn.response :json, :content_type => /\bjson$/
+        conn.adapter Faraday.default_adapter
       end
     end
 
